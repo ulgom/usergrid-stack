6d048948f5a9edb0d05fb9ef8a9341bf5f9e28ee
Todd Nine <tnine@apigee.com>
2012-11-02 12:37:54 -0700

    Revert "Merge pull request #202 from apigee/cassandra-upgrade"
    This reverts commit 3b86fc36ac7a94dc4bd46df84cbd70056edef6ba, reversing
    changes made to 866e5de4d04dcd45785da4b2eba9400aa010909c.

--- core/src/main/java/org/usergrid/persistence/cassandra/IndexBucketScanner.java
+++ core/src/main/java/org/usergrid/persistence/cassandra/IndexBucketScanner.java
@@ -143,7 +143,7 @@ public class IndexBucketScanner {
     }
     
     private static abstract class DynamicCompositeComparator implements Comparator<ByteBuffer>{
-        protected final AbstractType dynamicComposite;
+        protected final AbstractType<ByteBuffer> dynamicComposite;
         
         @SuppressWarnings("unchecked")
         protected DynamicCompositeComparator(ApplicationCF cf){
