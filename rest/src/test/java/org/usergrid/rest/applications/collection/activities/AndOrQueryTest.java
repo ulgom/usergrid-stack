f31c51a76cdc1683a15c159a6f81c557d55889f7
George Reyes <greyes@apigee.com>
2013-08-06 10:19:50 -0700

    Removed tests that did not use the activities collection

--- rest/src/test/java/org/usergrid/rest/applications/collection/activities/AndOrQueryTest.java
+++ rest/src/test/java/org/usergrid/rest/applications/collection/activities/AndOrQueryTest.java
@@ -1,7 +1,6 @@
 package org.usergrid.rest.applications.collection.activities;
 
 import org.codehaus.jackson.JsonNode;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.usergrid.rest.RestContextTest;
 import org.usergrid.rest.test.resource.CustomCollection;
@@ -104,56 +103,6 @@ public class AndOrQueryTest extends RestContextTest {
     String inCorrectQuery = "select * where verb = 'go' and ordinal >= 10 ";
 
     activities.verificationOfQueryResults(correctValues,true,inCorrectQuery);
-  }
-
-  @Test //Check to make sure that asc works
-  public void queryCheckAsc() throws Exception{
-
-    CustomCollection madeupStuff = collection("imagination");
-    Map character = hashMap("WhoHelpedYou","Ruff");
-
-    JsonNode[] correctValues = new JsonNode[1000];
-
-
-    correctValues = madeupStuff.createEntitiesWithOrdinal(character,1000);
-
-    String inquisitiveQuery = "select * where Ordinal gte 0 and Ordinal lte 2000 or WhoHelpedYou eq 'Ruff' ORDER BY " +
-        "Ordinal asc";
-
-    int totalEntitiesContained = madeupStuff.verificationOfQueryResults(correctValues,false,inquisitiveQuery);
-
-    assertEquals(1000,totalEntitiesContained);
-  }
-
-  @Ignore//Test to make sure all 1000 exist with a regular query
-  public void queryReturnCheck() throws Exception{
-    CustomCollection madeupStuff = collection("imagination");
-    Map character = hashMap("WhoHelpedYou","Ruff");
-
-    int numOfEntities = 1000;
-
-    JsonNode[] correctValues = madeupStuff.createEntitiesWithOrdinal(character,numOfEntities);
-
-    String inquisitiveQuery = "select * where Ordinal >= 0 and Ordinal <= 2000 or WhoHelpedYou = 'Ruff'";
-
-    int totalEntitiesContained = madeupStuff.verificationOfQueryResults(correctValues,true,inquisitiveQuery);
-
-    assertEquals(numOfEntities,totalEntitiesContained);
-  }
-
-  @Ignore
-  public void queryReturnCheckWithShortHand() {
-    CustomCollection madeupStuff = collection("imagination");
-    Map character = hashMap("WhoHelpedYou","Ruff");
-
-    madeupStuff.createEntitiesWithOrdinal(character,1000);
-
-    String inquisitiveQuery = "select * where Ordinal gte 0 and Ordinal lte 2000 or WhoHelpedYou eq 'Ruff'";
-
-    int totalEntitiesContained = madeupStuff.countEntities(inquisitiveQuery);
-
-    assertEquals(1000,totalEntitiesContained);
-
   }
 
 }
